e3566f271d454ad9222b08ca2ba3b44c1293ead3
Dakota Clark <bannaray95@gmail.com>
2019-02-22 05:24:48 +0000

    Automatically backed up by Learn

--- app/models/network.rb
+++ app/models/network.rb
@@ -3,8 +3,7 @@ class Network < ActiveRecord::Base
   has_many :chracters, through: :shows
   has_many :actors, through: :characters
   
-  def build_show
-    "#{self.call_letters}"
+
 
   def sorry
     "We're sorry about passing on John Mulaney's pilot"
