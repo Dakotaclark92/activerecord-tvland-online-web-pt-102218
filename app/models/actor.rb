230276f3a11f651d16a92be33aa89b58edd1333b
Dakota Clark <bannaray95@gmail.com>
2019-02-22 05:20:01 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -2,10 +2,10 @@ class Actor < ActiveRecord::Base
   has_many :characters
   has_many :shows, through: :characters
   
-  def full_name
-    "#{self.first_name} #{self.last_name}"
+
+
+  def list_roles
+    "#{self.characters[0].name} - #{self.characters[0].show.name}"
   end
-
-
   
 end
