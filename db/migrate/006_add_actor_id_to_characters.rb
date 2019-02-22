bf9e5f97fb57396cbd0fdda12986eaba30d61a6b
Dakota Clark <bannaray95@gmail.com>
2019-02-22 04:50:15 +0000

    Automatically backed up by Learn

--- db/migrate/006_add_actor_id_to_characters.rb
+++ db/migrate/006_add_actor_id_to_characters.rb
@@ -1,12 +1,5 @@
-class AddActorIDToCharacters < ActiveRecord::Migration[4.2]
+class AddActorIdToCharacters < ActiveRecord::Migration[4.2]
   
-  def change
-    add_column :characters, :actor_id, :integer
-  end
-end
-
-
-class AddActorIdToCharacters < ActiveRecord::Migration
   def change
     add_column :characters, :actor_id, :integer
   end
