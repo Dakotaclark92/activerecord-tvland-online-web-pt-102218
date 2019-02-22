ed1fd95cff23f1d1fc07753a53b44745545aec3a
Dakota Clark <bannaray95@gmail.com>
2019-02-22 05:05:03 +0000

    Automatically backed up by Learn

--- db/migrate/008_add_name_and_genre_to_shows.rb
+++ db/migrate/008_add_name_and_genre_to_shows.rb
@@ -1,6 +1,6 @@
-class AddBuildNetworkToShows < ActiveRecord::Migration[4.2]
+class AddNamesAndGenreToShows < ActiveRecord::Migration[4.2]
   
   def change
-    add_column :shows, :build_network, :string
+    add_column :shows, :genre, :string
   end
 end
